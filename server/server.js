b1ad0319a5cefbd22f8dfd07259fc779c180a855
Laurie-Anne Drapeau <Laurie-Anne.Drapeau@mcn.gouv.qc.ca>
2023-07-10 19:59:53 +0000

    backend + packets

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 
-const PORT = 8700
+const PORT = 8701
 //const fetch = require('node-fetch')
 const express = require('express')
 const app = express()
@@ -13,9 +13,34 @@ require('dotenv').config()
 
 
 
-app.get('/backend', (req, res) => {
+/*app.get('/backend', (req, res, next) => {
     res.send('Hello from the backend!');
-})
+})*/
 
-app.listen(8700, () => { console.log(`Server started on port ${8700}`) })
+
+
+async function getapi(req, res, next) {
+    try {
+        console.log("Fetching Data...");
+
+        const location = "paris";
+
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=ddcb7ebff34fe073865e71aa9b50c157`);
+
+        const data = await response.json();
+
+
+
+        res.send(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    }
+}
+
+
+app.get('/backend', getapi);
+
+
+app.listen(8701, () => { console.log(`Server started on port ${PORT}`) });
 
